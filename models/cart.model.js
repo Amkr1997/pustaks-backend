067c2a8d67790f0a6102f230a29c6570e577d6ab
Aman Khare <khareaman754@gmail.com>
2024-10-19 08:36:37 +0530

    chore: Updated cart model again

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,8 +1,24 @@
 const mongoose = require("mongoose");
 
 const cartSchema = mongoose.Schema({
-  bookId: {
+  bookName: {
     type: String,
+    required: true,
+  },
+  bookPrice: {
+    type: Number,
+    required: true,
+  },
+  bookImgUrl: {
+    type: String,
+    required: true,
+  },
+  bookCategory: {
+    type: String,
+    required: true,
+  },
+  bookRating: {
+    type: Number,
     required: true,
   },
   bookQuantity: {
